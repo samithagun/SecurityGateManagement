afa01c6c13afe58eb915063b0f5847f5f2da6a54
samithagun <samithax88@gmail.com>
2013-11-10 00:24:39 +0530

    Interface modified

--- PassIssueSystem/PassIssueSystem/App_Start/BundleConfig.cs
+++ PassIssueSystem/PassIssueSystem/App_Start/BundleConfig.cs
@@ -8,36 +8,35 @@ namespace PassIssueSystem
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.IgnoreList.Clear();
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui.js",
+                        "~/Scripts/jquery.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrapjs").Include(
+                        "~/Scripts/bootbox.min.js",
+                        "~/Scripts/wysihtml5.min.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/bootstrap-daterangepicker.js",
+                        "~/Scripts/bootstrap-wysihtml5.js",
+                        "~/Scripts/fullcalender.js",
+                        "~/Scripts/modernizr.min.js",
+                        "~/Scripts/moment.min.js",
+                        "~/Scripts/flot.min.js",
+                        "~/Scripts/excanvas.js",
+                        "~/Scripts/retina.js",
+                        "~/Scripts/theme.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new StyleBundle("~/Content/bootstrapcss").Include(
+                        "~/Content/Theme/bootbox.min.css",
+                        "~/Content/Theme/bootstrap.css",
+                        "~/Content/Theme/bootstrap-daterangepicker.css",
+                        "~/Content/Theme/bootstrap-wysihtml5.css",
+                        "~/Content/Theme/fullcalender.css",
+                        "~/Content/Theme/light-theme.css",
+                        "~/Content/Theme/theme-colors.css"));
         }
     }
 }
